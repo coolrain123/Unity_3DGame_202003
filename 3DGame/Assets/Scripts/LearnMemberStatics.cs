88ab2174938b85f75767617413ee98b2466f9159
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-03-26 21:33:29 +0800

    C#####

--- 3DGame/Assets/Scripts/LearnMemberStatics.cs
+++ 3DGame/Assets/Scripts/LearnMemberStatics.cs
@@ -8,11 +8,20 @@ public class LearnMemberStatics : MonoBehaviour
     bool d = false;
     string e = "字串";
 
+    public Camera cam;
+
     private void Start()
     {
         print(Random.value);
         print(Random.Range(0, 100));
 
         Time.timeScale = 0.5f;
+
+        print(Mathf.Abs(-999));
+        print(Mathf.PI);
+
+        print(cam.depth);
+        print("攝影機數量:" + Camera.allCamerasCount);
+        
     }
 }
