9e1b1db5a0cee769797e067907433778e18abda4
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-04-09 21:37:27 +0800

    完成載入畫面

---
+++ 3DGame/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("載入畫面")]
+    public GameObject panelLoading;
+    [Header("載入畫面文字")]
+    public Text textLoading;
+    [Header("載入畫面讀條")]
+    public Image imgLoading;
+
+
+   
+    public void StartLoading()
+    {
+        print("開始載入....");
+        panelLoading.SetActive(true);
+       
+        //SceneManager.LoadScene("關卡1");
+        StartCoroutine(Loading());
+    }
+
+    /// <summary>
+    /// 協程方法:載入
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Loading()
+    {
+       AsyncOperation ao = SceneManager.LoadSceneAsync("關卡1");
+
+        ao.allowSceneActivation = false; //是否自動載入畫面 = 否
+        while (ao.progress < 1)
+        {
+            print("關卡進度" + ao.progress);
+            yield return null;
+
+            textLoading.text = (ao.progress / 0.9 * 100).ToString("F2") + "  %";
+            imgLoading.fillAmount = ao.progress;
+
+            if (ao.progress == 0.9f)
+            ao.allowSceneActivation = true;
+        }
+    }
+}
