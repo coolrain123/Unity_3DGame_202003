6d10d00964fedc9ed47c6ea6630811ac32ff8a25
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-04-14 21:35:43 +0800

    角色移動攝影機追蹤

---
+++ 3DGame/Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public Transform Dragon;
+
+    [Header("追蹤速度"), Range(1, 100)]
+    public float speed = 1;
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    public void Track()
+    {
+        Vector3 drapos = Dragon.position;
+        drapos.y = 50;
+        drapos.z -= 10;
+
+        transform.position = Vector3.Lerp(transform.position, drapos, 0.4f * Time.deltaTime * speed);
+    }
+}
