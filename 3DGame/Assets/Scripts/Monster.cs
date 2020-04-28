4199cf6d68aecc35302a59099213f86251311e80
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-04-28 18:51:09 +0800

    123

---
+++ 3DGame/Assets/Scripts/Monster.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+
+public class Monster : MonoBehaviour
+{
+    [Header("怪物資料")]
+    public MonsterData Data;
+    
+    private Animator ani;
+    private float hp;
+
+    private void Start()
+    {
+        hp = Data.hp;
+       ani =  GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage">受到的傷害</param>
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+    public void Dead()
+    {
+        ani.SetBool("Dead",true);
+    }
+
+}
---
+++ 3DGame/Assets/Scripts/MonsterData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "怪物資料",menuName = "coolrain/怪物資料")]
+public class MonsterData : ScriptableObject
+{
+    [Header("攻擊力"), Range(0, 100000)]
+    public float atk;
+    [Header("血量"), Range(0, 100000)]
+    public float hp;
+    [Header("移動速度"), Range(0, 100000)]
+    public float speed;
+}
